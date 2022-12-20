b19d817e2832739806b9bb5e7ed070f97f5988dc
RafikTaieb <mtayeb.rafik@gmail.com>
2022-12-21 00:28:19 +0100

    fm

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import MainDash from './Components/MainDash/MainDash';
+import Sidebar from './Components/Sidebar/Sidebar';
+import RightSide from './Components/RightSide/RightSide';
+
+
+function App() {
+  return (
+    <div className="App">
+        <div className="AppGlass">
+          <Sidebar/>
+          <MainDash/>
+          <RightSide/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
+
